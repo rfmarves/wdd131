fc830e98bb349e5b7ec74ac04ece5f3960e176c2
Rene Marves <rfmarves@outlook.com>
2024-05-02 20:19:15 -0600

    consolidated to 1 js script

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,17 @@
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+// DOM elements for output
+const currentYearElement = document.querySelector("#currentYear");
+const lastModifiedElement = document.querySelector("#lastModified");
+
+// gets the date and year
+const today = new Date();
+let currentYear = today.getFullYear();
+
+// writes current year into the corresponding SPAN element
+currentYearElement.innerHTML = currentYear;
+
+// gets and writes when this document was last modified
+lastModifiedElement.innerHTML = document.lastModified;
