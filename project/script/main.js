7674b3027b5002db932cdbca798ee5b03ef9b95d
Rene Marves <rfmarves@outlook.com>
2024-06-01 12:24:34 -0600

    project checkpoint 1

---
+++ project/script/main.js
@@ -0,0 +1,17 @@
+// DOM elements
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+const currentYearElement = document.querySelector("#currentYear");
+
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+// gets the date and year
+const today = new Date();
+let currentYear = today.getFullYear();
+
+// writes current year into the corresponding SPAN element
+currentYearElement.innerHTML = currentYear;
